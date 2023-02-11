e20a9c0ec62ef4a1bea6e05632acd6063dde137b
Shubhee01 <shubhiji1111@gmail.com>
2023-02-11 16:08:04 +0530

    diabetes added

--- main.py
+++ main.py
@@ -11,7 +11,79 @@ from keras.preprocessing import image
 # from keras.preprocessing.image import ImageDataGenerator
 # from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from keras.utils import load_img, img_to_array
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+# ..................................................
+diabetes_dataset = pd.read_csv('diabetes.csv')
+diabetes_dataset.groupby('Outcome').mean()
+X = diabetes_dataset.drop(columns = 'Outcome', axis=1)
+Y = diabetes_dataset['Outcome']
+scaler = StandardScaler()
+scaler.fit(X)
+# standardized_data = scaler.transform(X)
+# X = standardized_data
+# Y = diabetes_dataset['Outcome']
+# X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
+# classifier = svm.SVC(kernel='linear')
+# classifier.fit(X_train, Y_train)
+# X_train_prediction = classifier.predict(X_train)
+# training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+# print('Accuracy score of the training data : ', training_data_accuracy)
+# X_test_prediction = classifier.predict(X_test)
+# test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+# print('Accuracy score of the test data : ', test_data_accuracy)
+# input_data = (1,100,72,19,173,25.8,0.587,18)
+
+# # changing the input_data to numpy array
+# input_data_as_numpy_array = np.asarray(input_data)
+
+# # reshape the array as we are predicting for one instance
+# input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+# # standardize the input data
+# std_data = scaler.transform(input_data_reshaped)
+# print(std_data)
+
+# prediction = classifier.predict(std_data)
+# print(prediction)
+
+# if (prediction[0] == 0):
+#     print('The person is not diabetic')
+# else:
+#     print('The person is diabetic')
+
+# import pickle
+# pickle.dump(classifier,open('diabetes_model.pkl','wb'))
+scaler = StandardScaler()
+scaler.fit(X)
+loaded_m=pickle.load(open('diabetes_model.pkl','rb'))
+input_data = (1,100,72,19,173,25.8,0.587,18)
+
+# changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+# # standardize the input data
+std_data = scaler.transform(input_data_reshaped)
+# print(std_data)
+
+prediction = loaded_m.predict(input_data_reshaped)
+# print(prediction)
+
+if (prediction[0] == 0):
+    print('The person is not diabetic')
+else:
+    print('The person is diabetic')
+
+#--------------------------------
 # import os
 # for dirname, _, filenames in os.walk('/kaggle/input'):
 #     for filename in filenames:
@@ -94,33 +166,46 @@ from keras.utils import load_img, img_to_array
 # print("THE UPLOADED X-RAY IMAGE IS: "+str(op))
 # model.save("model.h5")
 
-def bone_fracture():
-    loaded_model=load_model("best_model.h5")
-    img = load_img('image123.jpg',target_size=(224,224))
-    imag = img_to_array(img)
-    imaga = np.expand_dims(imag,axis=0) 
-    res=loaded_model.predict(imaga)
-    a=res[0]
-    if a<0.5:
-        op="Fracture"   
-    else:
-        op="Normal"
-    return "THE UPLOADED X-RAY IMAGE IS: "+str(op)
+# def bone_fracture():
+#     loaded_model=load_model("best_model.h5")
+#     img = load_img('image123.jpg',target_size=(224,224))
+#     imag = img_to_array(img)
+#     imaga = np.expand_dims(imag,axis=0) 
+#     res=loaded_model.predict(imaga)
+#     a=res[0]
+#     if a<0.5:
+#         op="Fracture"   
+#     else:
+#         op="Normal"
+#     return "THE UPLOADED X-RAY IMAGE IS: "+str(op)
 
+# def lung_disease():
+#     loaded_model=load_model("lungpred.h5",compile=False)
+#     img=load_img('lung_disease.jpg',target_size=(224,224))
+#     x=img_to_array(img)
+#     x=np.expand_dims(x,axis=0)
+#     img_data=preprocess_input(x)
+#     classes=loaded_model.predict(img_data)
+#     if classes[0][0]==0.0 and classes[0][1]==1.0:
+#         return "The person has pneumonia"
+#     elif classes[0][0]==1.0 and classes[0][1]==0.0:
+#         return "The person is normal"
+#     else:
+#         return "ille"
 
-def lung_disease():
-    loaded_model=load_model("lungpred.h5",compile=False)
-    img=load_img('lung_disease.jpg',target_size=(224,224))
-    x=img_to_array(img)
-    x=np.expand_dims(x,axis=0)
-    img_data=preprocess_input(x)
-    classes=loaded_model.predict(img_data)
-    if classes[0][0]==0.0 and classes[0][1]==1.0:
-        return "The person has pneumonia"
-    elif classes[0][0]==1.0 and classes[0][1]==0.0:
-        return "The person is normal"
-    else:
-        return "ille"
+# loaded_model=load_model("lungpred.h5",compile=False)
+# img=load_img('lung_disease.jpg',target_size=(224,224))
+# x=img_to_array(img)
+# x=np.expand_dims(x,axis=0)
+# img_data=preprocess_input(x)
+# classes=loaded_model.predict(img_data)
+# if round(classes[0][1])==1.0:
+#     print("The person has pneumonia")
+# elif round(classes[0][0])==1.0:
+#     print("The person is normal")
+# else:
+#     print("The person")
+# print(classes)
 
 # img = load_img('image123.jpg',target_size=(224,224))
 # print(type(img))
@@ -134,3 +219,24 @@ def lung_disease():
 # else:
 #     op="Normal"
 # print("THE UPLOADED X-RAY IMAGE IS: "+str(op))
+
+
+#Diabetes:----------------------------------------------------------------
+# loaded_model=pickle.load(open('diabetes_model2.pkl','rb'))
+# p=1
+# g=89
+# bp=66
+# st=23
+# insulin=94
+# bmi=28.1
+# dpf=0.167
+# age=21
+# input_data = (p,g,bp,st,insulin,bmi,dpf,age)
+# input_data_as_numpy_array = np.asarray(input_data)
+# input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+# res=loaded_model.predict(input_data_reshaped)
+# if (res == 0):
+#     print('The person is not diabetic')
+# else:
+#     print('The person is diabetic')
+# print(res)
