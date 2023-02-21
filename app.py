7d6bf07c96bf744a3276fcd065aa6a76805a1f9f
NikhilKD <nihilkd@gmail.com>
2023-02-21 13:40:05 +0530

    added

--- app.py
+++ app.py
@@ -6,13 +6,21 @@ import uuid
 from io import BytesIO
 from main import bone_fracture,lung_disease,diabetes_predict,insurance_pre,heart_prediction,mental_health
 from keys import config
+from flask_mail import Mail, Message
 import datetime
 import pdfkit
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///record.db'
 app.config['SQLALCHEMY_BINDS']={'data':'sqlite:///data.db','prediction':'sqlite:///prediction.db'}
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'nihilkd@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Shubhee@123'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
+mail = Mail(app)
 db=SQLAlchemy(app)
 app.config['UPLOAD_FOLDER'] = ''
 
@@ -312,13 +320,17 @@ def summary():
 def get_pdf():
     global predictions
     global dates
+    global user
     x = Record.query.filter_by(user_name=user).first()
     res=render_template('/pdf/index.html',predict=predictions,date=dates,profile=x)
     responsestring=pdfkit.from_string(res,False)
-    # print(type(responsestring))
     response=make_response(responsestring)
     response.headers['Content-Type']='application/pdf'
     response.headers['Content-Disposition']='inline; filename=report.pdf'
+    message = Message("Your Report", sender='nihilkd@gmail.com', recipients=[user])
+    message.attach('file.pdf', 'application/pdf', responsestring)
+
+    mail.send(message)
     return response
     # print(res)
     # return render_template('/pdf/index.html',predict=predictions,date=dates,profile=x)
