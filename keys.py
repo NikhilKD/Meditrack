0cba422176eca5d55a7a0c07dbfb9647b24e66e3
Shubhee01 <shubhiji1111@gmail.com>
2023-03-03 00:09:00 +0530

    added

---
+++ keys.py
@@ -0,0 +1,19 @@
+config={
+        'apiKey': "AIzaSyA0NrL3k15_rk4hQV2Mj1Ejf49GkECuaoQ",
+        'authDomain': "hideout-de60d.firebaseapp.com",
+        'databaseURL': "https://hideout-de60d-default-rtdb.firebaseio.com",
+        'projectId': "hideout-de60d",
+        'storageBucket': "hideout-de60d.appspot.com",
+        'messagingSenderId': "822673797712",
+        'appId': "1:822673797712:web:16624f9015f991d74e1755",
+        'measurementId': "G-NER9QNPQK4"
+}
+
+email={
+        "Id": "nihilkd@gmail.com",
+        "password": "olqmchficbqylrdn"
+}
+
+google={
+        "API_KEY":"AIzaSyC_5QA5DKXftkLgewUSrTuf3epPJ95MnAM"
+}
